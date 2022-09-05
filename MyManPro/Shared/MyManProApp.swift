345b922abb12e3e079f35c05e483eac8c6f5cc40
Lorenzo Malferrari <lorenzo.malferrari@natisoft.it>
2022-09-05 16:43:24 +0200

    Progetto creato
    E' stato creato il progetto. Il lavoro non inizierà. Il tutto è stato fatto per svolgere il task su Jira numero 932

---
+++ MyManPro/Shared/MyManProApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyManProApp.swift
+//  Shared
+//
+//  Created by Mac Studio Natisoft on 05/09/22.
+//
+
+import SwiftUI
+
+@main
+struct MyManProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
